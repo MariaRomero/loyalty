e31fd107e113844e7601ea05bd9a8b9b2b4d4363
mromero <maria.romero@news.co.uk>
2019-05-17 17:13:35 +0100

    A1: working on checking if merchantid on receipt matches schemes list

--- src/main/java/com/flux/test/MyImplementation.java
+++ src/main/java/com/flux/test/MyImplementation.java
@@ -59,16 +59,6 @@ public class MyImplementation implements ImplementMe {
 //            response.first().paymentsGiven shouldHaveSize 1
 //            response.first().paymentsGiven.first() shouldBe 100
 
-    //    Receipt(id=e6b1da05-050e-4463-b981-72793d4977ac,
-//            accountId=33f862b4-3f02-47e0-b9b7-d7e44cf5716e,
-//            merchantId=c8ed317f-c27d-4415-a80b-188b23047294,
-//            items=[
-//            Item(sku=1, price=100, quantity=1),
-//            Item(sku=2, price=200, quantity=1),
-//            Item(sku=1, price=100, quantity=1),
-//            Item(sku=1, price=100, quantity=1),
-//            Item(sku=1, price=100, quantity=1)])
-
     private List<List<String>> getSkuFromSchemeList(List<Scheme> schemeAvail) {
 
         List<List<String>> schemeSkus = new ArrayList<>();
@@ -93,19 +83,57 @@ public class MyImplementation implements ImplementMe {
 //                                                Item(sku=1, price=100, quantity=1),
 //                                                Item(sku=1, price=100, quantity=1),
 //                                                Item(sku=1, price=100, quantity=1)]
+
+        //        System.out.println(schemeSkus);  [[1, 2, 3, 4], [5, 6, 7, 8]]
+
         return receiptItemsList;
     }
+//
+//    private Integer numberOfStamps(List<Item> receiptItemsList) {
+//
+//    }
 
 
     public List calculateStamps(List<Scheme> schemeAvail, Receipt receipt) {
 
         List skusFromScheme = getSkuFromSchemeList(schemeAvail);
-        List itemFromReceipt = getItemsListFromReceipt(receipt);
+        List itemsFromReceipt = getItemsListFromReceipt(receipt);
+
+        for(int i = 0; i < itemsFromReceipt.size(); i++ ){
+            String skuOnReceipt = ((Item) itemsFromReceipt.get(i)).getSku();
+            for (int x = 0; x < ((ArrayList)skusFromScheme.get(i)).size(); x++ ){
+                if (((ArrayList) skusFromScheme.get(i)).get(x).equals(skuOnReceipt) {
+
+                }
+            }
+        }
 
         // compare items
-        itemFromReceipt.forEach((n) -> System.out.println(n));
+//        itemFromReceipt.forEach((n) -> System.out.println(n));
 
-        return itemFromReceipt;
+        return skusFromScheme;
+    }
+
+    //    Receipt(id=e6b1da05-050e-4463-b981-72793d4977ac,
+//            accountId=33f862b4-3f02-47e0-b9b7-d7e44cf5716e,
+//            merchantId=c8ed317f-c27d-4415-a80b-188b23047294,
+//            items=[
+//            Item(sku=1, price=100, quantity=1),
+//            Item(sku=2, price=200, quantity=1),
+//            Item(sku=1, price=100, quantity=1),
+//            Item(sku=1, price=100, quantity=1),
+//            Item(sku=1, price=100, quantity=1)])
+
+    private boolean schemesAvailForMerchant(List<Scheme> schemes, Receipt receipt) {
+        for (int i = 0; i < schemes.size(); i++ ) {
+            UUID schemeMerchantId = schemes.get(i).getMerchantId();
+            UUID receiptMerchantId = receipt.getMerchantId();
+            if (schemeMerchantId == receiptMerchantId) {
+               true;
+            } else {
+                false;
+            }
+        }
     }
 
     @NotNull
@@ -114,13 +142,14 @@ public class MyImplementation implements ImplementMe {
         List<Scheme> schemesAvail = getSchemes();
 //        System.out.println(schemeAvail);  [{"skus":["1","2","3","4"],"SchemeId":"10000000000","merchantId":"010101010101","maxStamps":"4"},
 //                                          {"skus":["5","6","7","8"],"SchemeId":"20000000000","merchantId":"010101010101","maxStamps":"4"}]
+        boolean lucky = schemesAvailForMerchant(schemesAvail, receipt);
 
         calculateStamps(schemesAvail, receipt);
         ApplyResponse response = new ApplyResponse(receipt.getMerchantId(), 1, 1, new ArrayList<>());
         List<ApplyResponse> responses = new ArrayList<>();
         responses.add(response);
 
-            return responses;
+        return responses;
     }
 
     @NotNull
